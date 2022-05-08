039810538d91194b02cbbcc2020e2768517bbf54
Avisheikbarua1158 <avisheik.barua@northsouth.edu>
2022-05-08 13:35:17 +0600

    testing

--- index.js
+++ index.js
@@ -56,10 +56,12 @@ async function run() {
         });
 
         // POST or ADD item
-        app.post('/items', async (req, res) => {
+        app.post('/items', (req, res) => {
             const newItem = req.body;
-            const result = await itemsCollection.insertOne(newItem);
-            res.send(result);
+            console.log('adding new user', newItem)
+
+            // const result = await itemsCollection.insertOne(newItem);
+            res.send({ result: 'success' });
 
 
         })
